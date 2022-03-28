adf8a166b1c73b5b084188bc70563d62d62dd95e
Kamal Hamza <khamsa.kamal48@gmail.com>
2022-03-28 14:01:11 +0530

    removed for loop to check for holidays

--- Login.py
+++ Login.py
@@ -16,46 +16,43 @@ load_dotenv()
 todays_date = datetime.now().strftime("%d %B %Y")
 todays_day = datetime.now().strftime("%A")
               
-with open('Holiday_List.csv', 'r') as file:
-    for holiday_list in file.readlines():
-        if todays_date in holiday_list or todays_day == "Sunday" or todays_day == "Saturday":
-            print ("It's a holiday today")
-            break
-        else:
-            # Pick a random number between 1 to 3600 seconds (within an hour)
-            random = randint(1, 3600)
+with open('Holiday_List.csv', 'r') as holiday_list:
+    if todays_date in holiday_list.read() or todays_day == "Sunday" or todays_day == "Saturday":
+        print ("It's a holiday today")
+    else:
+        # Pick a random number between 1 to 3600 seconds (within an hour)
+        random = randint(1, 3600)
 
-            # Sleep for the random number before logging into HRMS
-            time.sleep(random)
+        # Sleep for the random number before logging into HRMS
+        time.sleep(random)
 
-            # Retrieve contents from .env file
-            URL = os.getenv("URL")
-            LOGIN = os.getenv("LOGIN")
-            PASSWORD = os.getenv("PASSWORD")
+        # Retrieve contents from .env file
+        URL = os.getenv("URL")
+        LOGIN = os.getenv("LOGIN")
+        PASSWORD = os.getenv("PASSWORD")
 
-            driver = webdriver.Chrome(ChromeDriverManager().install())
+        driver = webdriver.Chrome(ChromeDriverManager().install())
 
-            # Open HRMS Page
-            driver.get(URL)
+        # Open HRMS Page
+        driver.get(URL)
 
-            # Wait
-            driver.implicitly_wait(15)
+        # Wait
+        driver.implicitly_wait(15)
 
-            # Enter Username
-            driver.find_element_by_id("txtUserName").send_keys(LOGIN)
+        # Enter Username
+        driver.find_element_by_id("txtUserName").send_keys(LOGIN)
 
-            # Enter password
-            driver.find_element_by_id ("txtPassword").send_keys(PASSWORD)
+        # Enter password
+        driver.find_element_by_id ("txtPassword").send_keys(PASSWORD)
 
-            # Click on the Login Button
-            driver.find_element_by_id("btnLogin").click()
-            
-            time.sleep(15)
+        # Click on the Login Button
+        driver.find_element_by_id("btnLogin").click()
+        
+        time.sleep(15)
 
-            # # Click on the Check-in/Check-out button
-            driver.find_element_by_id("btnCheckin").click()
-            
-            time.sleep(15)
+        # # Click on the Check-in/Check-out button
+        driver.find_element_by_id("btnCheckin").click()
+        
+        time.sleep(15)
 
-            driver.quit()
-            break
+        driver.quit()
